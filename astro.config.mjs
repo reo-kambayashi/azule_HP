91ff1edb6a340b037641cc7b7db0580426ea3b58
reo <kambayashi.reo.t6@dc.tohoku.ac.jp>
2025-05-06 23:42:06 +0900

    astro.config.mjsの整形とコメントの整理

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://reo-kambayashi.github.io',
     base: '/azule_HP/',
     output: "static",
     trailingSlash: 'always',
