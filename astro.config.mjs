34cc2b4086cf0372c500231e62f9abac8729fcb4
reo <kambayashi.reo.t6@dc.tohoku.ac.jp>
2025-05-06 23:36:14 +0900

    @astrojs/sitemapプラグインを追加し、astro.config.mjsを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // astro.config.mjs
 
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://reo-kambayashi.github.io',
     base: '/azule_HP/',
     output: "static",
-    trailingSlash: 'always'
+    trailingSlash: 'always',
+    site: 'https://reo-kambayashi.github.io/azule_HP',
+    integrations: [sitemap()],
 });
 
