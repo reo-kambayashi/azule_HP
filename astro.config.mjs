5685b8dc6fa6c123d3bf6a88d4f8de864fe3fb55
reo <kambayashi.reo.t6@dc.tohoku.ac.jp>
2025-05-06 16:41:31 +0900

    astro.config.mjsのbaseパスにスラッシュを追加し、404.htmlを新規作成。home.astroで画像パスを動的に設定。

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: 'https://reo-kambayashi.github.io',
-    base: '/azule_HP',
-    output: "static"
+    base: '/azule_HP/',
+    output: "static",
+    trailingSlash: 'always'
 });
 
